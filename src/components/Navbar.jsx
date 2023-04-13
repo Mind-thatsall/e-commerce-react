3bcecb2a9bc0a291c6451b5f4ede6dd4ef0babe8
Mathieu Rossi <mathieurossi77@gmail.com>
2023-04-13 23:15:19 +0200

    Added: Fully responsive product page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav
-      className="fixed flex justify-between w-full px-[6vw] mt-[1.25vh] text-[1vw]"
+      className="z-[2] mix-blend-difference text-[#796B66] fixed flex justify-between w-full px-[6vw] mt-[1.25vh] text-[4vw] md:text-[2vw] lg:text-[1vw]"
       style={{ fontFamily: "ClashDisplay-Medium" }}
     >
       <ul className="flex gap-[2vw]">
@@ -20,7 +20,8 @@ const Navbar = () => {
         </li>
       </ul>
       <p className="hidden lg:block" style={{ wordSpacing: "0.4vw" }}>
-        CGU CONTACT | 13:30 - 06/04/2023 | MADE IN FRANCE
+        13:30 - 06/04/2023 | <Link to="/cgu">CGU</Link>{" "}
+        <Link to="/contact">CONTACT</Link> | MADE IN FRANCE
       </p>
       <ul className="flex gap-[2vw]">
         <li className="hidden lg:block">SEARCH</li>
