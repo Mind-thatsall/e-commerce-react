662f804c0a22cd0e1f8d4f0ffb0882f11143848c
Mathieu Rossi <mathieurossi77@gmail.com>
2023-04-13 15:25:12 +0200

    Added: Categories page is now fully responsive

--- src/views/Categories.jsx
+++ src/views/Categories.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import categorie from "@/assets/images/men.jpg";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const Categories = () => {
+  const scrollBoxRef = useRef(null);
+
+  function maxSize() {
+    console.log("test");
+    scrollBoxRef.current.style.height =
+      window.innerHeight - scrollBoxRef.current.offsetTop + "px";
+  }
+
+  useEffect(() => {
+    maxSize();
+    window.addEventListener("resize", maxSize);
+
+    return () => {
+      window.removeEventListener("resize", maxSize);
+    };
+  }, []);
+
   return (
     <div className="px-[4vw] pt-[10vh] flex flex-col justify-center items-center">
       <h2
@@ -13,17 +32,20 @@ const Categories = () => {
         CATEGORIES
       </h2>
       <span className="w-[98%] block h-1 bg-[#796B66] mix-blend-difference"></span>
-      <div className="hide-scroll grid grid-cols-4 gap-[1.3vw] overflow-auto h-[76vh] py-[3vh] w-fit mx-auto">
-        <img src={categorie} alt="" width={300} height={450} />
-        <img src={categorie} alt="" width={300} height={450} />
-        <img src={categorie} alt="" width={300} height={450} />
-        <img src={categorie} alt="" width={300} height={450} />
-        <img src={categorie} alt="" width={300} height={450} />
-        <img src={categorie} alt="" width={300} height={450} />
-        <img src={categorie} alt="" width={300} height={450} />
-        <img src={categorie} alt="" width={300} height={450} />
-        <img src={categorie} alt="" width={300} height={450} />
-        <img src={categorie} alt="" width={300} height={450} />
+      <div
+        ref={scrollBoxRef}
+        className="hide-scroll grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[1.3vw] overflow-auto py-[3vh] w-[96%] mx-auto"
+      >
+        <img src={categorie} alt="" width={310} height={450} />
+        <img src={categorie} alt="" width={310} height={450} />
+        <img src={categorie} alt="" width={310} height={450} />
+        <img src={categorie} alt="" width={310} height={450} />
+        <img src={categorie} alt="" width={310} height={450} />
+        <img src={categorie} alt="" width={310} height={450} />
+        <img src={categorie} alt="" width={310} height={450} />
+        <img src={categorie} alt="" width={310} height={450} />
+        <img src={categorie} alt="" width={310} height={450} />
+        <img src={categorie} alt="" width={310} height={450} />
       </div>
     </div>
   );
