0ba8989a17f3db5d6169063af1e40ad0bfb806a8
Mathieu Rossi <mathieurossi77@gmail.com>
2023-04-14 11:03:25 +0200

    Added: Cart page

--- src/views/Article.jsx
+++ src/views/Article.jsx
@@ -4,7 +4,7 @@ import article from "@/assets/images/men.jpg";
 const Article = () => {
   return (
     <div className="md:px-[4vw] h-screen relative flex flex-col md:grid md:grid-cols-8 lg:grid-cols-12 grid-rows-6">
-      <div className="relative w-full h-20vh md:h-auto md:col-start-1 md:row-start-1 md:col-end-5 lg:col-end-7 md:row-end-7">
+      <div className="relative w-full h-20vh md:h-auto md:col-start-1 md:row-start-1 md:col-end-9 lg:col-end-7 md:row-end-5 lg:row-end-7">
         <img
           className="w-full h-[60vh] md:h-full md:static"
           src={article}
@@ -13,14 +13,14 @@ const Article = () => {
           height={450}
         />
         <img
-          className="w-full h-[60vh] md:h-full absolute md:static top-0"
+          className="w-full h-[60vh] md:h-full absolute top-0"
           src={article}
           alt="black hoodie"
           width={310}
           height={450}
         />
         <img
-          className="w-full h-[60vh] md:h-full absolute md:static top-0"
+          className="w-full h-[60vh] md:h-full absolute top-0"
           src={article}
           alt="black hoodie"
           width={310}
@@ -29,13 +29,13 @@ const Article = () => {
       </div>
       <h2
         style={{ fontFamily: "ClashDisplay-Bold" }}
-        className="ml-[4vw] mt-[4vw] w-max md:col-start-4 md:col-end-7 md:ml-[5vw] lg:ml-0 md:row-start-1 md:row-end-3 lg:col-start-6 lg:col-end-10 lg:row-start-1 lg:row-end-3 md:self-end text-[8vw] mix-blend-difference text-[#796B66] md:w-min leading-[7vw]"
+        className="ml-[4vw] mt-[4vw] w-max md:col-start-1 md:col-end-4 md:mt-[2vh] md:ml-[2vw] lg:ml-0 md:row-start-5 md:row-end-6 lg:col-start-6 lg:col-end-10 lg:row-start-1 lg:row-end-3 lg:self-end text-[8vw] leading-[6vw] mix-blend-difference text-[#796B66] lg:w-min lg:leading-[7vw]"
       >
         BLACK HOODIE
       </h2>
       <p
         style={{ fontFamily: "ClashDisplay-Medium" }}
-        className="text-[3.5vw] ml-[3vw] mt-[4vw] md:ml-0 md:mt-0 leading-[4.5vw] md:text-[1.5vw] md:leading-[2.2vw] self-center lg:text-[1.2vw] lg:leading-[2vw] row-start-3 row-end-4 md:col-start-5 md:col-end-9 lg:col-start-7 lg:col-end-13 md:w-[90%] pl-[2vw] mix-blend-difference text-[#796B66]"
+        className="text-[3.5vw] ml-[3vw] mt-[4vw] md:ml-0 md:mb-6 lg:mb-0 md:mt-0 leading-[4.5vw] md:text-[2vw] md:leading-[2.2vw] lg:self-center lg:text-[1.2vw] lg:leading-[2vw] md:row-start-5 md:row-end-6 lg:row-start-3 lg:row-end-4 md:col-start-1 md:col-end-6 md:self-end lg:col-start-7 lg:col-end-13 md:w-full lg:w-[90%] pl-[2vw] mix-blend-difference text-[#796B66]"
       >
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam dolorem
         culpa, voluptatibus porro esse ex explicabo, hic laboriosam illum dolor
@@ -43,14 +43,14 @@ const Article = () => {
       </p>
       <p
         style={{ fontFamily: "ClashDisplay-SemiBold" }}
-        className="text-[5vw] ml-[3vw] mt-[2vw] md:ml-0 md:mt-0 md:text-[5vw] md:leading-[5vw] lg:text-[4.6vw] lg:leading-[5vw] row-start-4 row-end-5 lg:col-start-7 lg:col-end-10 pl-[2vw] mix-blend-difference text-[#796B66]"
+        className="text-[5vw] ml-[3vw] mt-[2vw] md:ml-0 md:mt-[1.5vh] lg:mt-0 md:text-[5vw] md:leading-[5vw] lg:text-[4.6vw] lg:leading-[5vw] md:row-start-6 md:row-end-7 md:col-start-1 md:mb-[2vh] lg:mb-0 lg:self-start md:col-end-5 lg:row-start-4 lg:row-end-5 lg:col-start-7 lg:col-end-10 pl-[2vw] mix-blend-difference text-[#796B66]"
       >
         20.00$
       </p>
 
       <div
         style={{ fontFamily: "ClashDisplay-Medium" }}
-        className="text-[5vw] px-[4vw] md:text-[1.2vw] absolute bottom-[2vh] w-full md:static flex flex-col md:block md:row-start-5 md:row-end-7 md:self-end md:col-start-5 md:col-end-9 lg:col-start-7 lg:col-end-10 md:pl-[2vw] md:pb-[2vw] text-[#222421]"
+        className="text-[5vw] px-[4vw] md:text-[1.2vw] absolute bottom-[2vh] w-full md:static flex flex-col md:block md:row-start-6 md:row-end-7 md:self-end md:col-start-1 md:col-end-5 lg:col-start-7 lg:col-end-10 md:pl-[2vw] md:pb-[2vw] text-[#222421]"
       >
         <p className="mb-[1vh]">XL / L / M / S / XS</p>
         <button
