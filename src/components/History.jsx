4b9c2ca8c3ee1620a2e1cae5a29e027b6cb8117b
Mathieu Rossi <mathieurossi77@gmail.com>
2023-04-18 14:49:29 +0200

    Fix: styling on inputs and other components

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -3,43 +3,43 @@ import React from "react";
 const History = () => {
   return (
     <div style={{ fontFamily: "ClashDisplay-Medium" }} className="mt-6 text-xl">
-      <div className="flex flex-col sm:flex-row text-center">
-        <div className="sm:w-1/2 px-15 py-2 font-bold bg-gray-200">N°</div>
-        <div className="sm:w-1/2 px-8 py-2 font-bold bg-gray-200">Items</div>
-        <div className="sm:w-1/2 px-5 py-2 font-bold bg-gray-200">Date</div>
-        <div className="sm:w-1/2 px-2 py-2 font-bold bg-gray-200"> Total</div>
+      <div className="flex flex-col text-center sm:flex-row">
+        <div className="sm:w-1/2 px-15 py-2 font-bold bg-[#222421] text-[#9F938B]">N°</div>
+        <div className="px-8 py-2 font-bold bg-[#222421] text-[#9F938B] sm:w-1/2">Items</div>
+        <div className="px-5 py-2 font-bold bg-[#222421] text-[#9F938B] sm:w-1/2">Date</div>
+        <div className="px-2 py-2 font-bold bg-[#222421] text-[#9F938B] sm:w-1/2"> Total</div>
       </div>
       <div className="flex flex-col sm:flex-row">
-        <div className="sm:w-1/2 px-10 py-2">#2588528855</div>
-        <div className="sm:w-1/2 px-5 py-2">5</div>
-        <div className="sm:w-1/2 px-10 py-2">15/03/2023</div>
-        <div className="sm:w-1/2 px-10 py-2">150€</div>
+        <div className="px-10 py-2 sm:w-1/2">#2588528855</div>
+        <div className="px-5 py-2 sm:w-1/2">5</div>
+        <div className="px-10 py-2 sm:w-1/2">15/03/2023</div>
+        <div className="px-10 py-2 sm:w-1/2">150€</div>
       </div>
       <div className="flex flex-col sm:flex-row">
-        <div className="sm:w-1/2 px-10 py-2">#2588528855</div>
-        <div className="sm:w-1/2 px-5 py-2">5</div>
-        <div className="sm:w-1/2 px-10 py-2">15/03/2023</div>
-        <div className="sm:w-1/2 px-10 py-2">150€</div>
+        <div className="px-10 py-2 sm:w-1/2">#2588528855</div>
+        <div className="px-5 py-2 sm:w-1/2">5</div>
+        <div className="px-10 py-2 sm:w-1/2">15/03/2023</div>
+        <div className="px-10 py-2 sm:w-1/2">150€</div>
       </div>
       <div className="flex flex-col sm:flex-row">
-        <div className="sm:w-1/2 px-10 py-2">#2588528855</div>
-        <div className="sm:w-1/2 px-5 py-2">5</div>
-        <div className="sm:w-1/2 px-10 py-2">15/03/2023</div>
-        <div className="sm:w-1/2 px-10 py-2">150€</div>
+        <div className="px-10 py-2 sm:w-1/2">#2588528855</div>
+        <div className="px-5 py-2 sm:w-1/2">5</div>
+        <div className="px-10 py-2 sm:w-1/2">15/03/2023</div>
+        <div className="px-10 py-2 sm:w-1/2">150€</div>
       </div>
       <div className="flex flex-col sm:flex-row">
-        <div className="sm:w-1/2 px-10 py-2">#2588528855</div>
-        <div className="sm:w-1/2 px-5 py-2">5</div>
-        <div className="sm:w-1/2 px-10 py-2">15/03/2023</div>
-        <div className="sm:w-1/2 px-10 py-2">150€</div>
+        <div className="px-10 py-2 sm:w-1/2">#2588528855</div>
+        <div className="px-5 py-2 sm:w-1/2">5</div>
+        <div className="px-10 py-2 sm:w-1/2">15/03/2023</div>
+        <div className="px-10 py-2 sm:w-1/2">150€</div>
       </div>
       <div className="flex flex-col sm:flex-row">
-        <div className="sm:w-1/2 px-10 py-2">#2588528855</div>
-        <div className="sm:w-1/2 px-5 py-2">5</div>
-        <div className="sm:w-1/2 px-10 py-2">15/03/2023</div>
-        <div className="sm:w-1/2 px-10 py-2">150€</div>
+        <div className="px-10 py-2 sm:w-1/2">#2588528855</div>
+        <div className="px-5 py-2 sm:w-1/2">5</div>
+        <div className="px-10 py-2 sm:w-1/2">15/03/2023</div>
+        <div className="px-10 py-2 sm:w-1/2">150€</div>
       </div>
-      
+
     </div>
   );
 };
