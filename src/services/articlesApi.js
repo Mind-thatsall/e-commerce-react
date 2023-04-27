0e0dafbf72d4d23b8304c246329b2e691daa1817
Mathieu Rossi <mathieurossi77@gmail.com>
2023-04-27 13:48:39 +0200

    Fix: new errors for some cases when registering or when the backend is not available

--- src/services/articlesApi.js
+++ src/services/articlesApi.js
@@ -4,24 +4,36 @@ const articlesApi = axios.create({
   baseURL : import.meta.env.VITE_API_URL ,
 });
 
-export const articlesApiEndPoint = "/api/products";
+export const articlesApiEndPoint = "api/products";
 
 // Functions used to interact with the articles on our backend, here we're only fetching because the backend take care of the rest
 
 export const getArticles = async () => {
   try {
     const response = await articlesApi.get(articlesApiEndPoint);
+    console.log(response.data)
     return response.data;
   } 
   catch (error) {
     console.error(error)
-    if(err.code === "ERR_NETWORK")throw new Error("An error occured while getting articles")
+    if(err.code === "ERR_NETWORK") throw new Error("An error occured while getting articles")
   }
 };
 
-export const getArticlesFromCategory = async (gender, slug) => {
+export const getArticlesFromCategory = async (gender, id) => {
   try {
-    const response = await articlesApi.get(`/api/${gender}/categories/${slug}/products`);
+    const response = await articlesApi.get(`api/${gender}/categories/${id}/products`);
+    return response.data;
+  } 
+  catch (error) {
+    console.error(error)
+    if(error.code === "ERR_NETWORK")throw new Error("An error occured while getting articles")
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const response = await articlesApi.get(`api/categories`);
     return response.data;
   } 
   catch (error) {
@@ -32,7 +44,7 @@ export const getArticlesFromCategory = async (gender, slug) => {
 
 export const getArticle = async (id) => {
   try {
-    const response = await articlesApi.get(`/api/products/${id}`);
+    const response = await articlesApi.get(`api/products/${id}`);
     return response.data;
   } 
   catch (error) {
